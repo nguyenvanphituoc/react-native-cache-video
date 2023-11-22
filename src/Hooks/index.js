7a22888631efd4238d242652a33168b5d64f4bdc
liberty <liberty@vinova.com.sg>
2023-11-22 10:39:27 +0700

    chore: remove js file

--- src/Hooks/index.js
+++
@@ -1,3 +0,0 @@
-export * from './useProxyCacheProvider';
-export * from './useIsForeground';
-export * from './useCache';
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { CacheVideoHttpProxy } from './Provider';
-export { CacheManager } from './ProxyCacheManager';
-export * from './Provider';
-export * from './Hooks';
-export * from './Utils/util';
-export * from './Utils/constants';
-export function multiply(a, b) {
-    return CacheVideoHttpProxy.multiply(a, b);
-}
