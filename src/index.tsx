0a98266bec09f92caaa143e0f038f84ab1169b20
liberty <liberty@vinova.com.sg>
2023-11-27 16:58:17 +0700

    fix: remove multiply function

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,5 @@
-import { CacheVideoHttpProxy } from './Provider';
 export { CacheManager } from './ProxyCacheManager';
 export * from './Provider';
 export * from './Hooks';
 export * from './Utils/util';
 export * from './Utils/constants';
-
-export function multiply(a: number, b: number): Promise<number> {
-  return CacheVideoHttpProxy.multiply(a, b);
-}
